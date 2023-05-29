ffb34c093ade27c8e794a327a803145f66a88165
Paula Santoyo <a01745312@itesm.mx>
2023-05-29 12:06:12 -0600

    Conexión a BD

--- Aplicativo/settings.py
+++ Aplicativo/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'Aplicativo.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'quizapp4',
+        'USER': 'admin',
+        'PASSWORD': 'password',
+        'HOST': 'database-2.c0fd1vp0jts0.us-east-1.rds.amazonaws.com',
+        'PORT': '3306',
     }
 }
 
