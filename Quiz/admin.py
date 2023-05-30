ca9d1feb4009376ab64a2158e13634bf6761a389
André <84719490+gggandre@users.noreply.github.com>
2023-05-30 01:33:28 -0600

    a

--- Quiz/admin.py
+++ Quiz/admin.py
@@ -1,40 +1,21 @@
 from django.contrib import admin
 
-from .models import Pregunta, ElegirRespuesta, PreguntasRespondidas, QuizUsuario, Quiz
-
-from .forms import ElegirInlineFormset
-
+from .models import Pregunta, ElegirRespuesta, Quiz, QuizUsuario, PreguntasRespondidas
 
 class ElegirRespuestaInline(admin.TabularInline):
     model = ElegirRespuesta
-    can_delete = False
-    max_num = ElegirRespuesta.MAXIMO_RESPUESTA
-    min_num = ElegirRespuesta.MAXIMO_RESPUESTA
-    formset = ElegirInlineFormset
-
+    extra = 4
 
 class PreguntaAdmin(admin.ModelAdmin):
-    model = Pregunta
-    inlines = (ElegirRespuestaInline,)
-    list_display = ['texto', ]
-    search_fields = ['texto', 'preguntas__texto']
+    inlines = [ElegirRespuestaInline]
 
-
-class PreguntasRespondidasAdmin(admin.ModelAdmin):
-    list_display = ['pregunta', 'respuesta', 'correcta', 'puntaje_obtenido']
-
-
-class QuizInline(admin.StackedInline):
-    model = Quiz
-    extra = 0
-
+class QuizInline(admin.TabularInline):
+    model = QuizUsuario.quizes.through
 
 class QuizUsuarioAdmin(admin.ModelAdmin):
     inlines = [QuizInline]
 
-
-admin.site.register(PreguntasRespondidas, PreguntasRespondidasAdmin)
 admin.site.register(Pregunta, PreguntaAdmin)
-admin.site.register(ElegirRespuesta)
-admin.site.register(QuizUsuario, QuizUsuarioAdmin)
 admin.site.register(Quiz)
+admin.site.register(QuizUsuario, QuizUsuarioAdmin)
+admin.site.register(PreguntasRespondidas)
