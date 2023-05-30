87f96932b8b5f0b4cb3e66d2f72d52f9ae74a50f
André <84719490+gggandre@users.noreply.github.com>
2023-05-30 08:57:02 -0600

    admin choose questions

--- Quiz/admin.py
+++ Quiz/admin.py
@@ -4,28 +4,46 @@ from .models import Pregunta, ElegirRespuesta, PreguntasRespondidas, QuizUsuario
 
 from .forms import ElegirInlineFormset
 
+
 class ElegirRespuestaInline(admin.TabularInline):
-	model = ElegirRespuesta
-	can_delete =False
-	max_num = ElegirRespuesta.MAXIMO_RESPUESTA
-	min_num = ElegirRespuesta.MAXIMO_RESPUESTA
-	formset = ElegirInlineFormset
+    model = ElegirRespuesta
+    can_delete = False
+    max_num = ElegirRespuesta.MAXIMO_RESPUESTA
+    min_num = ElegirRespuesta.MAXIMO_RESPUESTA
+    formset = ElegirInlineFormset
+
+
 
 class PreguntaAdmin(admin.ModelAdmin):
-	model = Pregunta
-	inlines = (ElegirRespuestaInline, )
-	list_display = ['texto',]
-	search_fields = ['texto', 'preguntas__texto']
-
+    model = Pregunta
+    inlines = (ElegirRespuestaInline, )
+    list_display = ['texto',]
+    search_fields = ['texto', 'preguntas__texto']
 
 class PreguntasRespondidasAdmin(admin.ModelAdmin):
-	list_display = ['pregunta', 'respuesta', 'correcta', 'puntaje_obtenido']
+    list_display = ['pregunta', 'respuesta', 'correcta', 'puntaje_obtenido']
 
-	class Meta:
-		model = PreguntasRespondidas
+    class Meta:
+        model = PreguntasRespondidas
 
 
-admin.site.register(PreguntasRespondidas)
+@admin.register(QuizUsuario)
+class QuizUsuarioAdmin(admin.ModelAdmin):
+    list_display = ['usuario', 'puntaje_total']
+    search_fields = ['usuario__username']
+    actions = ['generar_preguntas']
+
+    def generar_preguntas(self, request, queryset):
+        cantidad_preguntas = int(input("Ingrese la cantidad de preguntas que desea generar: "))
+        for quiz_user in queryset:
+            preguntas = quiz_user.obtener_nuevas_preguntas(cantidad_preguntas)
+            if preguntas:
+                for pregunta in preguntas:
+                    quiz_user.crear_intentos(pregunta)
+
+    generar_preguntas.short_description = "Generar preguntas para los usuarios seleccionados"
+
+
+admin.site.register(PreguntasRespondidas, PreguntasRespondidasAdmin)
 admin.site.register(Pregunta, PreguntaAdmin)
 admin.site.register(ElegirRespuesta)
-admin.site.register(QuizUsuario)
